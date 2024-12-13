d33a9c86012718259fa761dcdfa3f9f3eaaac7b0
Lukasz Went <zakzyne@gmail.com>
2024-12-13 20:41:18 +0100

    BA-0 Project init

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as proxy from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(
-    '/api',
-    proxy.createProxyMiddleware({
-      target: 'https://swapi.dev/api',
-      changeOrigin: true,
-    }),
-  );
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
